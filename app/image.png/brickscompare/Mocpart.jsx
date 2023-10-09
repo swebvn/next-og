bec592c538b56b0126559501a550af418be0db77
bangnokia <bangnokia@gmail.com>
2023-10-10 01:35:22 +0700

    wip

--- app/image.png/brickscompare/Mocpart.jsx
+++ app/image.png/brickscompare/Mocpart.jsx
@@ -6,12 +6,12 @@ export default function Mocpart({ params, host }) {
     <div tw="flex w-full h-full items-center justify-center">
       <img src={`http://${host}/brickscompare/moc-part-bg.png`} tw="absolute w-full h-full" />
 
-      <div tw="absolute top-[100px] px-[200px] w-full flex flex-col items-center justify-center h-[800px]">
+      <div tw="absolute top-[70px] px-[150px] w-full flex flex-col items-center justify-center h-[530px]">
         <img src={image} tw="w-full h-full" />
       </div>
 
-      <div tw="absolute top-[980px] px-[50px] w-full flex flex-col items-center justify-center h-[100px]">
-        <div tw="text-center text-4xl">{productName}</div>
+      <div tw="absolute top-[650px] px-[35px] w-full flex flex-col items-center justify-center h-[70px]">
+        <div tw="text-center text-2xl">{productName}</div>
       </div>
     </div>
   )
