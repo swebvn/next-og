9a41a69df2c32a6d34a5a4aba92f7115c1ae2726
bangnokia <bangnokia@gmail.com>
2024-09-11 15:53:48 +0700

    wip

--- app/image.png/route.jsx
+++ app/image.png/route.jsx
@@ -8,23 +8,23 @@ export async function GET(request) {
   const { searchParams, host } = new URL(request.url);
   const template = searchParams.get('template');
 
-  const fontData = await fetch(
+  const fontBeVietnam = await fetch(
     new URL('../../assets/fonts/BeVietnamPro-Medium.woff', import.meta.url)
   ).then(res => res.arrayBuffer());
 
   let Component = null;
 
   if (template === 'demo') {
-    return new ImageResponse(<Demo params={searchParams} host={host} />, { width: 800, height: 600 });
+    return new ImageResponse(<Demo params={searchParams} />, { width: 800, height: 600 });
   }
 
   if (template === 'tung') {
-    return new ImageResponse(<Tung params={searchParams} host={host} />, {
+    return new ImageResponse(<Tung params={searchParams} />, {
       width: 800,
       height: 600,
       fonts: [{
         name: "BeVietnamPro",
-        data: fontData,
+        data: fontBeVietnam,
         style: "normal",
       } ]
     });
