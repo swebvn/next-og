bfe6b59ab98d11f576bce176385addfbc21d48ec
Đặng Đình Tùng <81344999+DangTung0310@users.noreply.github.com>
2024-09-13 15:42:21 +0700

    Add files via upload
    push new

--- app/image.png/templates/Tung.jsx
+++ app/image.png/templates/Tung.jsx
@@ -1,32 +1,148 @@
 export default function Demo({ params }) {
-    const title = params.get('title') || 'Blank title'
-    const image = params.get('image')
-    const logo = params.get('logo')
-
+    const bg_image = params.get('bg_image')
+    const image_charm = params.get('image_charm')
+    const title1 = params.get('title1') || 'Blank title'
+    const title2 = params.get('title2') || 'Blank title'
+    const product_image1 = params.get('product_image1')
+    const product_image2 = params.get('product_image2')
+    const product_image3 = params.get('product_image3')
+    const product_image4 = params.get('product_image4')
+    const product_image5 = params.get('product_image5')
+    const product_image6 = params.get('product_image6')
+    const product_image7 = params.get('product_image7')
+    const product_image8 = params.get('product_image8')
+    const product_image9 = params.get('product_image9')
+    const product_image10 = params.get('product_image10')
+    const product_image11 = params.get('product_image11')
+    const product_image12 = params.get('product_image12')
     return (
         <div tw="flex w-full h-full items-center justify-center">
-
-
             <div tw="absolute w-full h-full flex flex-col items-center justify-center" >
-                <img src={image} tw="w-full h-full" />
+                <img src={bg_image} tw="w-full h-full" />
             </div>
 
-            <img src={logo} alt="" style={{
+            <img src={image_charm} alt="" style={{
                 position: 'absolute',
-                top: '20px',
-                left: '20px',
-                width: '100px',
-                height: '100px',
-                transform: 'rotate(69deg)',
+                top: '188px',
+                left: '310px',
+                width: '880px',
+                height: '1123px',
+                transform: 'rotate(0deg)',
             }} />
 
-            <h1 tw="text-center absolute top-[20px] text-red-500 text-[69px] font-bold"
+            <img src={image_bg_charm} alt="" style={{
+                position: 'absolute',
+                top: '368px',
+                left: '358px',
+                width: '783px',
+                height: '697px',
+                transform: 'rotate(0deg)',
+            }} />
+
+            <img src={product_image1} alt="" style={{
+                position: 'absolute',
+                top: '595px',
+                left: '440px',
+                width: '211px',
+                height: '213px',
+                transform: 'rotate(-38.62deg)',
+            }} />
+
+            <img src={product_image2} alt="" style={{
+                position: 'absolute',
+                top: '552px',
+                left: '531px',
+                width: '211px',
+                height: '212px',
+                transform: 'rotate(-18.7deg)',
+            }} />
+             <img src={product_image3} alt="" style={{
+                position: 'absolute',
+                top: '545px',
+                left: '1007px',
+                width: '209px',
+                height: '210px',
+                transform: 'rotate(-111deg)',
+            }} />     
+            <img src={product_image4} alt="" style={{
+                position: 'absolute',
+                top: '630px',
+                left: '903px',
+                width: '219px',
+                height: '220px',
+                transform: 'rotate(8.5deg)',
+            }} />       
+            <img src={product_image5} alt="" style={{
+                position: 'absolute',
+                top: '757px',
+                left: '508px',
+                width: '207px',
+                height: '208px',
+                transform: 'rotate(21deg)',
+            }} />       
+            <img src={product_image6} alt="" style={{
+                position: 'absolute',
+                top: '630px',
+                left: '673px',
+                width: '203px',
+                height: '205px',
+                transform: 'rotate(0deg)',
+            }} />       
+            <img src={product_image7} alt="" style={{
+                position: 'absolute',
+                top: '801px',
+                left: '537px',
+                width: '202px',
+                height: '203px',
+                transform: 'rotate(-51deg)',
+            }} />       
+            <img src={product_image8} alt="" style={{
+                position: 'absolute',
+                top: '882px',
+                left: '581px',
+                width: '196px',
+                height: '198px',
+                transform: 'rotate(0deg)',
+            }} />       
+            <img src={product_image9} alt="" style={{
+                position: 'absolute',
+                top: '997pxx',
+                left: '945px',
+                width: '201px',
+                height: '202px',
+                transform: 'rotate(-152deg)',
+            }} />       
+            <img src={product_image10} alt="" style={{
+                position: 'absolute',
+                top: '818px',
+                left: '870px',
+                width: '211px',
+                height: '212px',
+                transform: 'rotate(-9.5deg)',
+            }} />       
+            <img src={product_image11} alt="" style={{
+                position: 'absolute',
+                top: '676px',
+                left: '924px',
+                width: '211px',
+                height: '212px',
+                transform: 'rotate(-69deg)',
+            }} />                   
+
+            <h1 tw="text-center absolute top-[1103px] left-[597px] text-black-500 text-[60px] font-bold"
                 style={{
                     fontFamily: 'BeVietnamPro',
                     color: 'cyan',
                     fontFamily: 'Arial',
                 }}
-            >{title}</h1>
+            >{title1}</h1>
+               <h1 tw="text-center absolute top-[1181px] left-[488px] text-black-500 text-[60px] font-bold"
+                style={{
+                    fontFamily: 'BeVietnamPro',
+                    color: 'cyan',
+                    fontFamily: 'Arial',
+                }}
+            >{title2}</h1>
         </div>
     )
 }
