8ad0c85cb2745989f70ce6c4ce6b9b47f718de72
Đặng Đình Tùng <81344999+DangTung0310@users.noreply.github.com>
2024-11-06 17:20:03 +0700

    Add files via upload
    update

--- app/image.png/templates/duc-template1.jsx
+++ app/image.png/templates/duc-template1.jsx
@@ -1,21 +1,20 @@
-export default function Demo({ params }) {
+export default function duc_template1({ params }) {
     const image_goc1 = params.get('image_goc1')
     const bg_goc1 = params.get('bg_goc1')
     return (
-        <div tw="flex w-full h-full items-center justify-center">
+        <div tw="relative flex w-full h-full items-center justify-center">
+            <img src={bg_goc1} alt="" style={{
+                position: 'absolute',
+                top: '240px',
+                left: '150px',
+                width: '700px',
+                height: '470px', 
+                transform: 'skewX(0deg) skewY(4deg) rotate(12deg)', 
+            }} />
             <div tw="absolute w-full h-full object-cover flex flex-col items-center justify-center">
                 <img src={image_goc1} tw="w-full h-full" />
             </div>
-            <div tw="flex">
-            <img src={bg_goc1} alt="" style={{
-                position: 'absolute',
-                top: 'calc(50% - 260px)',
-                left: 'calc(50% - 350px)',
-                width: '700px',
-                height: '470px', 
-                transform: 'rotate3d(1, 0, 10, 15deg)',
-            }} />
-            </div>
+
         </div>
     )
 }
