7453645217aa14a924b46bffc9c110a94b15146b
Đặng Đình Tùng <81344999+DangTung0310@users.noreply.github.com>
2024-11-06 16:07:48 +0700

    update

--- app/image.png/templates/duc-template1.jsx
+++ app/image.png/templates/duc-template1.jsx
@@ -1,20 +1,21 @@
 export default function Demo({ params }) {
-    const bg_goc1 = params.get('image_charm')
-    const image_goc1 = params.get('image_charm')
+    const image_goc1 = params.get('image_goc1')
+    const bg_goc1 = params.get('bg_goc1')
     return (
         <div tw="flex w-full h-full items-center justify-center">
-            <div tw="absolute w-full h-full flex flex-col items-center justify-center" >
-                <img src={bg_goc1} tw="w-full h-full" />
-            </div> 
+            <div tw="absolute w-full h-full object-cover flex flex-col items-center justify-center">
+                <img src={image_goc1} tw="w-full h-full" />
+            </div>
+            <div tw="flex">
+            <img src={bg_goc1} alt="" style={{
+                position: 'absolute',
+                top: 'calc(50% - 260px)',
+                left: 'calc(50% - 350px)',
+                width: '700px',
+                height: '470px', 
+                transform: 'rotate3d(1, 0, 10, 15deg)',
+            }} />
+            </div>
         </div>
-
-        <img src={image_goc1} alt="" style={{
-            position: 'absolute',
-            top: 'calc(50% - 260px)',
-            left: 'calc(50% - 350px)',
-            width: '700px',
-            height: '470px', 
-            transform: 'rotate3d(1, 0, 10, 15deg)',
-        }} />
     )
-} 
+}
