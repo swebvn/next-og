a7200d0913ea4fd91dd858f555cb48646beaf8e6
Đặng Đình Tùng <81344999+DangTung0310@users.noreply.github.com>
2024-11-07 16:06:36 +0700

    Add files via upload
    update route

--- app/image.png/route.jsx
+++ app/image.png/route.jsx
@@ -4,6 +4,11 @@ import Tung from './templates/Tung';
 import Charm from './templates/charm';
 import Duc_template1 from './templates/duc-template1';
 import Duc_template2 from './templates/duc-template2';
+import Duc_template3 from './templates/duc-template3';
+import Duc_template4 from './templates/duc-template4';
+import Duc_template5 from './templates/duc-template5';
+import Duc_template6 from './templates/duc-template6';
+import Duc_template7 from './templates/duc-template7';
 export const runtime = 'edge';
 export async function GET(request) {
   const { searchParams, host } = new URL(request.url);
@@ -57,6 +62,62 @@ export async function GET(request) {
 
   if (template === 'duc-template2') {
     return new ImageResponse(<Duc_template2 params={searchParams} />, {
+      width: 1000, 
+      height: 1000,
+      fonts: [{
+        name: "BeVietnamPro",
+        data: fontBeVietnam,
+        style: "normal",
+      } ]
+    });
+  }
+
+  if (template === 'duc-template3') {
+    return new ImageResponse(<Duc_template3 params={searchParams} />, {
+      width: 1000, 
+      height: 1000,
+      fonts: [{
+        name: "BeVietnamPro",
+        data: fontBeVietnam,
+        style: "normal",
+      } ]
+    });
+  }
+  if (template === 'duc-template4') {
+    return new ImageResponse(<Duc_template4 params={searchParams} />, {
+      width: 1000, 
+      height: 1000,
+      fonts: [{
+        name: "BeVietnamPro",
+        data: fontBeVietnam,
+        style: "normal",
+      } ]
+    });
+  }
+  if (template === 'duc-template5') {
+    return new ImageResponse(<Duc_template5 params={searchParams} />, {
+      width: 1000, 
+      height: 1000,
+      fonts: [{
+        name: "BeVietnamPro",
+        data: fontBeVietnam,
+        style: "normal",
+      } ]
+    });
+  }
+  if (template === 'duc-template6') {
+    return new ImageResponse(<Duc_template6 params={searchParams} />, {
+      width: 1000, 
+      height: 1000,
+      fonts: [{
+        name: "BeVietnamPro",
+        data: fontBeVietnam,
+        style: "normal",
+      } ]
+    });
+  }
+  if (template === 'duc-template7') {
+    return new ImageResponse(<Duc_template7 params={searchParams} />, {
       width: 1000, 
       height: 1000,
       fonts: [{
