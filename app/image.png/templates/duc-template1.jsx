edeb780a3c647f50a90850fdd8d21e03eeca5b74
Đặng Đình Tùng <81344999+DangTung0310@users.noreply.github.com>
2024-11-08 12:04:45 +0700

    Add files via upload
    update

--- app/image.png/templates/duc-template1.jsx
+++ app/image.png/templates/duc-template1.jsx
@@ -5,11 +5,11 @@ export default function duc_template1({ params }) {
         <div tw="relative flex w-full h-full items-center justify-center">
             <img src={bg_goc1} alt="" style={{
                 position: 'absolute',
-                top: '255px',
-                left: '150px',
-                width: '710px',
-                height: '440px', 
-                transform: 'skewX(0deg) skewY(-2deg) rotate(0deg)', 
+                top: '275px',
+                left: '115px',
+                width: '780px', 
+                height: '400px', 
+                transform: 'skewX(-38deg) skewY(12deg) rotate(0deg)', 
             }} />
             <div tw="absolute w-full h-full object-cover flex flex-col items-center justify-center">
                 <img src={image_goc1} tw="w-full h-full" />
