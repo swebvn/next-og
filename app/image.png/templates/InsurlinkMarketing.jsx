b0710e20bfd197303c20ea4ba2b1822e44082593
bangnokia <bangnokia@gmail.com>
2024-11-20 17:16:19 +0700

    support avatar

--- app/image.png/templates/InsurlinkMarketing.jsx
+++ app/image.png/templates/InsurlinkMarketing.jsx
@@ -2,6 +2,7 @@ export default function InsurlinkMarketing({ params }) {
     const background = params.get('bg') || 'https://bomfy.customedge.co/storage/media/f8bab50a-179f-41d8-b877-29d00780ff0b.jpg'
     const name = params.get('name')
     const phone = params.get('phone')
+    const avatar = params.get('avatar') || 'https://bomfy.customedge.co/storage/media/50030921-28bd-45d2-9a4f-c30252188819.png'
     const left = '125px';
 
     return (
@@ -10,6 +11,15 @@ export default function InsurlinkMarketing({ params }) {
             <div tw="absolute w-full h-full object-cover flex flex-col items-center justify-center">
                 <img src={background} tw="w-full h-full" />
             </div>
+
+            <img src={avatar} alt="" style={{
+                width: '78px',
+                height: '78px',
+                top: '407px',
+                left: '16px',
+                borderRadius: '100%',
+                position: 'absolute',
+            }} />
 
             <div tw="font-bold" style={{
                 position: 'absolute',
