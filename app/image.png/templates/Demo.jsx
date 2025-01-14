643a3dbe55d3e4e01080a77f70af7b3808af74b6
bangnokia <bangnokia@gmail.com>
2025-01-14 14:16:24 +0700

    setup placeholder

--- app/image.png/templates/Demo.jsx
+++ app/image.png/templates/Demo.jsx
@@ -1,13 +1,13 @@
 export default function Demo({ params }) {
     const title = params.get('title') || 'Title'
-    const image = params.get('image')
-    const image1 = params.get('image1')
+    const image = params.get('image') || 'https://placehold.co/200x200.png'
+    const image1 = params.get('image1') || 'https://placehold.co/100x100.png'
 
     return (
         <div tw="flex w-full h-full items-center justify-center">
 
             <div tw="absolute w-full h-full object-cover flex flex-col items-center justify-center">
-                <img src={image} tw="w-full h-full" />
+                <img src={image} tw="w-full h-full" width="200" height="200" />
             </div>
 
             <div tw="flex">
