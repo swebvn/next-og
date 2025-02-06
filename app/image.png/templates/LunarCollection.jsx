48c8b7e7f895ad4129e007d8d42220c1650ac22e
bangnokia <bangnokia@gmail.com>
2025-02-06 18:13:33 +0700

    update lunar collection

--- app/image.png/templates/LunarCollection.jsx
+++ app/image.png/templates/LunarCollection.jsx
@@ -1,20 +1,27 @@
 export default function LunarCollection({ params }) {
-    const image = 'https://remove-bg.customedge.co/lunar-merch.b-cdn.net/ouranhighschoolhostclub.store/media/150/ssrco,mhoodie,mens,fafafa:ca443f4786,front,square_product,x600-bg,f8f8f8.1.jpg';
-    const text = "The Simpson T-shirt"
+    const image = params.get('image') || 'https://remove-bg.customedge.co/lunar-merch.b-cdn.net/ouranhighschoolhostclub.store/media/150/ssrco,mhoodie,mens,fafafa:ca443f4786,front,square_product,x600-bg,f8f8f8.1.jpg';
+    const text = params.get('text') || "Ouran High School Host Club Cases"
+    const circle = "https://lunar-merch.b-cdn.net/000-assets/circle.png"
 
 
     return (
-        <div tw="absolute inset-0 flex items-center justify-center border border-[10px] border-rose-500">
-            <img
-                tw="-mt-[20px] w-[220px] h-[220px] object-cover rounded-full border border-2 border-gray-900"
-                src={image} />
-            <div style={{
-                position: 'absolute',
-                bottom: '5px',
-                fontSize: '15px',
-                textTransform: 'uppercase',
-                fontWeight: 900,
-            }}>{text}</div>
+        <div tw="absolute inset-0 flex items-center justify-center">
+            <div tw="flex relative w-[220px] h-[220px] p-[15px]">
+                <img src={circle} tw="absolute w-[220px] h-[220px] inset-0 object-cover" />
+                <img
+                    tw="w-full h-full object-cover rounded-full"
+                    src={image} />
+            </div>
+
+            {text ? (
+                <div style={{
+                    position: 'absolute',
+                    bottom: '5px',
+                    fontSize: '15px',
+                    textTransform: 'uppercase',
+                    fontWeight: 900,
+                }}>{text}</div>
+            ) : null}
         </div>
     )
 }
