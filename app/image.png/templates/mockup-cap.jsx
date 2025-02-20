325020529db8a1b6e7b77989f4d06beb19a78a94
Đặng Đình Tùng <81344999+DangTung0310@users.noreply.github.com>
2025-02-20 17:24:06 +0700

    add new template

---
+++ app/image.png/templates/mockup-cap.jsx
@@ -0,0 +1,27 @@
+export default function mockup_cap({ params }) {
+    const bg_image = params.get('bg_image')
+    const image_1 = params.get('image_1')
+    return (
+        <div tw="relative flex w-full h-full items-center justify-center">
+            <div tw="absolute w-full h-full object-cover flex flex-col items-center justify-center">
+                <img src={bg_image} tw="w-full h-full" />
+            </div>
+            <div tw="absolute flex flex-col items-center justify-center" 
+            style={{
+                top: '50%',
+                left: '50%',
+                width: '250px',
+                height: '200px',
+                transform: 'translateX(-50%) translateY(-63%)',
+            }}>
+            <img src={image_1} alt="" 
+             style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+            }}
+            />
+            </div>
+    </div>
+    )
+}
