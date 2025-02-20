083f1b58cde14b035b2bceab77c6f97116d3655d
Đặng Đình Tùng <81344999+DangTung0310@users.noreply.github.com>
2025-02-20 15:38:43 +0700

    add new mockup

---
+++ app/image.png/templates/mockup-poster.jsx
@@ -0,0 +1,27 @@
+export default function mockup_lunar1({ params }) {
+    const bg_image = params.get('bg_image')
+    const image_1 = params.get('image_1')
+    return (
+        <div tw="relative flex w-full h-full items-center justify-center">
+            <div tw="absolute w-full h-full object-cover flex flex-col items-center justify-center">
+                <img src={bg_image} tw="w-full h-full" />
+            </div>
+            <div tw="absolute flex flex-col items-center justify-center" 
+            style={{
+                top: '50%',
+                left: '50%',
+                width: '594px',
+                height: '840px',
+                transform: 'translateX(-50.3%) translateY(-49.2%)',
+            }}>
+            <img src={image_1} alt="" 
+             style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+            }}
+            />
+            </div>
+    </div>
+    )
+}
