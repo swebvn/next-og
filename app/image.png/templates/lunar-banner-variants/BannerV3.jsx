2b23eb6c5a4c3af11808ac8495a675e1ec170e3f
bangnokia <bangnokia@gmail.com>
2025-06-24 09:30:28 +0700

    wip

--- app/image.png/templates/lunar-banner-variants/BannerV3.jsx
+++ app/image.png/templates/lunar-banner-variants/BannerV3.jsx
@@ -172,7 +172,7 @@ export default function BannerV3({ image_1, image_2, image_3, image_4, text_1, t
                     letterSpacing: '1px',
                     boxShadow: '0 6px 20px rgba(255,107,53,0.4)',
                 }}>
-                    🔥 {text_2} - UP TO 50% OFF
+                    {text_2}
                 </div>
 
                 {/* Social Proof */}
@@ -210,7 +210,9 @@ export default function BannerV3({ image_1, image_2, image_3, image_4, text_1, t
                         alignItems: 'center',
                         justifyContent: 'center',
                     }}>
-                        🛒 SHOP NOW
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+</svg> SHOP NOW
                     </div>
                 </div>
             </div>
